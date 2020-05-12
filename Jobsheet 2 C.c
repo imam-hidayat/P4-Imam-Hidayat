e923cad24db4b0f5dd07acdbcdba4a8f6844b544
imam-hidayat <64600014+imam-hidayat@users.noreply.github.com>
2020-05-12 19:56:00 +0700

    Create Jobsheet 2 C.c
    Mengisi dan menampilkan elemen array (dinamis)

---
+++ Jobsheet 2 C.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+main()
+{
+	printf("Nama: Imam Hidayat");
+	printf("\nNim: F1B019070");
+	printf("\nKelompok: 14\n");
+	
+	int x[10];
+	printf("Masukkan nilai ke-1: ");
+	scanf("%i",&x[0]);
+	printf("Masukkan nilai ke-2: ");
+	scanf("%i",&x[1]);
+	printf("Masukkan nilai ke-3: ");
+	scanf("%i",&x[2]);
+	printf("Masukkan nilai ke-4: ");
+	scanf("%i",&x[3]);
+	printf("Masukkan nilai ke-5: ");
+	scanf("%i",&x[4]);
+	printf("Masukkan nilai ke-6: ");
+	scanf("%i",&x[5]);
+	printf("Masukkan nilai ke-7: ");
+	scanf("%i",&x[6]);
+	printf("Masukkan nilai ke-8: ");
+	scanf("%i",&x[7]);
+	printf("Masukkan nilai ke-9: ");
+	scanf("%i",&x[8]);
+	printf("Masukkan nilai ke-10: ");
+	scanf("%i",&x[9]);
+	
+	printf("\nNilai ke-1= %i",x[0]);
+	printf("\nNilai ke-2= %i",x[1]);
+	printf("\nNilai ke-3= %i",x[2]);
+	printf("\nNilai ke-4= %i",x[3]);
+	printf("\nNilai ke-5= %i",x[4]);
+	printf("\nNilai ke-6= %i",x[5]);
+	printf("\nNilai ke-7= %i",x[6]);
+	printf("\nNilai ke-8= %i",x[7]);
+	printf("\nNilai ke-9= %i",x[8]);
+	printf("\nNilai ke-10= %i",x[9]);
+
+}

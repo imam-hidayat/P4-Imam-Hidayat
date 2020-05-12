39984147f98c033a96b6a537381e7b420991ba89
imam-hidayat <64600014+imam-hidayat@users.noreply.github.com>
2020-05-12 20:05:28 +0700

    Create Jobsheet 1 C++.cpp
    Menampilkan array satu dimensi

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+	cout<<"\nNama : Imam Hidayat";
+	cout<<"\nNIM: F1B019070";
+	cout<<"\nkelompok: 14\n";
+	int my_array[10] = {10,9,8,7,6,5,4,3,2,1};
+	cout<<"NIM ke-1 = "<<my_array[0]<<endl;
+	cout<<"NIM ke-2 = "<<my_array[1]<<endl;
+	cout<<"NIM ke-3 = "<<my_array[2]<<endl;
+	cout<<"NIM ke-4 = "<<my_array[3]<<endl;
+	cout<<"NIM ke-5 = "<<my_array[4]<<endl;
+	cout<<"NIM ke-6 = "<<my_array[5]<<endl;
+	cout<<"NIM ke-7 = "<<my_array[6]<<endl;
+	cout<<"NIM ke-8 = "<<my_array[7]<<endl;
+	cout<<"NIM ke-9 = "<<my_array[8]<<endl;
+	cout<<"NIM ke-10 = "<<my_array[9]<<endl;
+}
